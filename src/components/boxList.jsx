3a0dce74e269c7cd8b04e66707cb0d3e61ef6617
Victor-Avilla <victorfonsecaavila@gmail.com>
2025-11-05 20:12:05 -0300

    configuracoes iniciais

---
+++ src/components/boxList.jsx
@@ -0,0 +1,17 @@
+export default function BoxList(inputValue) {
+
+    const [list, setList] = useState([''])
+
+
+
+    return (
+        <article>
+            <label> lista</label>
+            <ul>
+                { }
+            </ul>
+
+        </article>
+
+    )
+}
