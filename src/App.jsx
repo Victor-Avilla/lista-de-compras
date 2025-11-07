b839dcce0cb66f399a9a2400fd18f4bee9aaf1cc
Victor-Avilla <victorfonsecaavila@gmail.com>
2025-11-06 23:32:51 -0300

    feat: input com useState e componente de lista estilizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import BoxList from "./components/boxList"
+import { useState } from "react"
+
+
 
 function App() {
-
   const [item, setItem] = useState('')
+  const [list, setList] = useState([])
+
+
+
+
+  function addItem() {
+    setList([...list, {
+      id: Math.random(),
+      item
+    }])
+    return
+  }
+
 
 
   return (
     <main className='bg-blackPin h-screen'>
       <header className='flex justify-center items-center h-16 bg-orangePin'><h1 className='text-beigePin text-5xl font-caveat'>Minha Lista de Compras</h1></header>
-      <input type='text' placeholder='Adicione um item' onChange={setItem(item.target.value)} className='flex h-10 w-xl m-auto my-10 pl-2 rounded-lg bg-grayPin placeholder:text-beigePin focus:outline-none text-beigePin ' />
+      <input type='text' value={item} placeholder='Adicione um item' onChange={(e) => setItem(e.target.value)}
+        className='inline-block align-middle h-10 w-2xs max-w-2xs m-auto my-10 pl-2 rounded-lg bg-grayPin placeholder:text-beigePin focus:outline-none text-beigePin ' />
+      <button onClick={addItem} className="inline-block align-middle h-10 my-10 w-14 max-w-20 m-auto px-2 rounded-lg bg-grayPin text-beigePin">Adicionar</button>
 
-      <BoxList items={items}/>
-
+      <BoxList inputValue={list} />
+      {console.log(item)}
+      {console.log(list)}
     </main>
   )
 }
--- src/components/boxList.jsx
+++ src/components/boxList.jsx
@@ -1,14 +1,11 @@
-export default function BoxList(inputValue) {
-
-    const [list, setList] = useState([''])
-
-
-
+export default function BoxList({inputValue}) {
     return (
-        <article>
-            <label> lista</label>
+        <article className="bg-grayPin text-beigePin max-w-96 max-h-96 h-full rounded-lg p-5 m-5">
+            <label> Comida</label>
             <ul>
-                { }
+                {inputValue.map((obj)=>(
+                    <li key={obj.id}>{obj.item}</li>
+                ))}
             </ul>
 
         </article>
