661c196ab633deb309505babe2a807bf4ac7b295
Victor-Avilla <victorfonsecaavila@gmail.com>
2025-11-11 21:22:18 -0300

    feat: adicionado botão delete e fit content ao BoxList; style: gradiente linear no estillo global

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import BoxList from "./components/BoxList"
+import BoxList from "./components/boxList"
 import { useState } from "react"
 
 function App() {
@@ -17,25 +17,32 @@ function App() {
     return
   }
 
+  function deleteItem(itemId) {
+    const newList = list.filter((e) => e.id !== itemId)
+    setList(newList)
+    console.log(newList)
+  }
+
+
   return (
-    <main className='bg-blackPin h-screen'>
-      <header className='flex justify-center items-center h-16 bg-orangePin'><h1 className='text-beigePin text-5xl font-caveat'>Minha Lista de Compras</h1></header>
+    <main className='bg-linear-to-b from-grayPin to-blackPin h-screen'>
+      <header className='flex justify-center items-center h-16 bg-orangePin drop-shadow-2xl rounded-b-lg'><h1 className='text-beigePin text-5xl font-caveat'>Minha Lista de Compras</h1></header>
 
-      <div className="flex justify-center max-w-2xl mx-auto w-auto">
+      <div className="flex justify-center max-w-2xl mx-auto w-auto drop-shadow-2xl">
         <input type='text' value={item} placeholder='Adicione um item' onChange={(e) => setItem(e.target.value)}
-          className='align-middle h-10 w-auto max-w-2xs my-10 pl-2 rounded-l-lg bg-grayPin placeholder:text-beigePin focus:outline-none text-beigePin ' />
+          className='bg-linear-to-b from-grayPin to-blackPin align-middle h-10 w-auto max-w-2xs my-10 pl-2 rounded-l-lg bg-grayPin placeholder:text-gray-40 focus:outline-none text-beigePin ' />
 
-        <select id="category" defaultValue={"comida"} title="Escolha uma categoria" className="align-middle h-10 w-auto max-w-2xs my-10 pl-2 bg-grayPin placeholder:text-beigePin focus:outline-none text-beigePin" name="category">
+        <select id="category" defaultValue={"comida"} title="Escolha uma categoria" className="bg-linear-to-b from-grayPin to-blackPin align-middle h-10 w-auto max-w-2xs my-10 pl-2 bg-grayPin placeholder:text-beigePin focus:outline-none text-beigePin" name="category">
           <option value="comida">Comida</option>
           <option value="limpeza">Limpeza</option>
           <option value="outros">Outros</option>
         </select>
 
-        <button onClick={addItem} className="align-middle h-10 my-10 w-auto max-w-20 px-2 rounded-r-lg bg-grayPin text-beigePin">Adicionar</button>
+        <button onClick={addItem} className="bg-linear-to-b from-grayPin to-blackPin align-middle h-10 my-10 w-auto max-w-20 px-2 rounded-r-lg bg-grayPin text-beigePin"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg></button>
       </div>
 
-      <BoxList inputValue={list} />
-      {console.log(list)}
+      <BoxList inputValue={list} deleteItem={deleteItem} />
+
     </main>
   )
 }
--- src/components/boxList.jsx
+++ src/components/boxList.jsx
@@ -1,17 +1,16 @@
-export default function BoxList({inputValue, category}) {
 
+export default function BoxList({ inputValue, deleteItem, category }) {
 
 
     return (
-        <article className="bg-grayPin text-beigePin drop-shadow-2xl max-w-96 max-h-96 h-full rounded-lg p-5 m-5">
+        <article className=" bg-linear-to-b from-grayPin to-blackPin text-beigePin drop-shadow-2xl max-w-96 max-h-96 h-fit rounded-3xl p-5 m-5">
             <label id="Comida" className="text-2xl bold"> Comida</label>
             <ul>
-                {inputValue.map((obj)=>(
-                    <li key={obj.id}>{obj.item}</li>
+                {inputValue.map((obj) => (
+                    <li key={obj.id}>{obj.item}<button onClick={() => deleteItem(obj.id)} className=" float-right rounded-lg w-5 h-5 hover:text-red-600 ">—</button></li>
                 ))}
+
             </ul>
-
         </article>
-
     )
 }
